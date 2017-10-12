eef3b45d50a255d04c70f3c365cff6fcef7a0f79
Kieran Eglin <kieran.eglin@gmail.com>
2017-10-11 18:09:15 -0700

    switching computers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+
 const CHARSET: &str = "349ACEFHJKLMNPRTUVWXYabefghkpqstuxyz";
+// REFERENCE          "0123456789abcdefghijklmnopqrstuvwxyz"
+
 // Not included:
 // B8G6I1l0OoQDS5Z72ij (obvious)
 // rn -> m,vv -> w, cl -> d
@@ -6,40 +11,23 @@ const CHARSET: &str = "349ACEFHJKLMNPRTUVWXYabefghkpqstuxyz";
 fn main() {
     // println!("{:?}", "test".char_at(0));
     // encode(0);
-    let test = encode(43324234234234);
+    let test = encode(73);
     println!("{:?}", test);
 
     let t2 = decode(&test);
-    println!("{:?}", t2);
+    // println!("{:?}", t2);
     // println!("{:?}", usize::from_str_radix(&test, 36));
 }
 
-fn encode(num: usize) -> String {
-    let encoded: Vec<char> = base36encode(num).chars().collect();
-    let allowed_chars: Vec<char> = CHARSET.chars().collect();
-    let mut result: String = "".to_owned();
+fn encode(mut num: usize) -> String {
+    let chars: Vec<char> = CHARSET.chars().collect();
 
-    for ch in encoded {
-        let index = usize::from_str_radix(&ch.to_string(), 36).unwrap();
-        result += &allowed_chars[index].to_string()
-    }
-
-    result
-}
-
-fn decode(string: &str) -> usize {
-    println!("{:?}", CHARSET.find("a"));
-    2
-}
-
-fn base36encode(mut num: usize) -> String {
     if num == 0 {
-        return 0.to_string();
+        return chars[0].to_string();
     }
 
     let mut encoded: String = "".to_owned();
     let mut remainder: usize;
-    let chars: Vec<char> = "0123456789abcdefghijklmnopqrstuvwxyz".chars().collect();
 
     while num > 0 {
         remainder = num % chars.len();
@@ -49,6 +37,54 @@ fn base36encode(mut num: usize) -> String {
 
     encoded
 }
+
+fn decode(string: &str) -> usize {
+    let chars: Vec<char> = string.chars().collect();
+    let base = 10;
+
+    for ch in chars {
+        println!("{:?}", CHARSET.find(ch));
+    }
+
+    // println!("{:?}", CHARSET.find("a"));
+    2
+}
+
+// fn base36encode(mut num: usize) -> String {
+//     if num == 0 {
+//         return 0.to_string();
+//     }
+//
+//     let mut encoded: String = "".to_owned();
+//     let mut remainder: usize;
+//     let chars: Vec<char> = "0123456789abcdefghijklmnopqrstuvwxyz".chars().collect();
+//
+//     while num > 0 {
+//         remainder = num % chars.len();
+//         num = num / chars.len();
+//         encoded = chars[remainder].to_string() + &encoded;
+//     }
+//
+//     encoded
+// }
+// fn base36encode(mut num: usize) -> String {
+//     if num == 0 {
+//         return 0.to_string();
+//     }
+//
+//     let mut encoded: String = "".to_owned();
+//     let mut remainder: usize;
+//     let chars: Vec<char> = "0123456789abcdefghijklmnopqrstuvwxyz".chars().collect();
+//
+//     while num > 0 {
+//         remainder = num % chars.len();
+//         num = num / chars.len();
+//         encoded = chars[remainder].to_string() + &encoded;
+//     }
+//
+//     encoded
+// }
+
 
 // def base36encode(integer):
 //     chars, encoded = '0123456789abcdefghijklmnopqrstuvwxyz', ''
